77d981f424ea51d681d382ead05610e17005c39b
Rafael Machado <pigao_linkin@hotmail.com>
2023-02-14 15:34:17 -0300

    a

---
+++ ApiDeposito/src/test/java/br/com/bling/ApiDeposito/ApiDepositoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bling.ApiDeposito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiDepositoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
