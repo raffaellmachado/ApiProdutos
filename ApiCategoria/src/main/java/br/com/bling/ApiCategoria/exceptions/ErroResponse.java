490df26973d406428fc715643e02b823330a4cbb
Rafael Machado <pigao_linkin@hotmail.com>
2023-03-09 21:25:14 -0300

    09/03

---
+++ ApiCategoria/src/main/java/br/com/bling/ApiCategoria/exceptions/ErroResponse.java
@@ -0,0 +1,17 @@
+package br.com.bling.ApiCategoria.exceptions;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class ErroResponse {
+
+    @JsonProperty("erro")
+    private DetalhesErroResponse erro;
+}
