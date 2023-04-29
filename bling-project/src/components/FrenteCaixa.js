8a8d5b8b82a3e251bcf1e349e8eb3b5b517f9ccb
Rafael Machado <pigao_linkin@hotmail.com>
2023-04-29 00:57:37 -0300

    29/04

--- bling-project/src/components/FrenteCaixa.js
+++ bling-project/src/components/FrenteCaixa.js
@@ -12,37 +12,47 @@ import Spinner from 'react-bootstrap/Spinner';
 
 import '../css/FrenteCaixa.css';
 import { parse } from 'js2xmlparser';
+import Suggest from 'react-autosuggest';
+
 
 
 class FrenteCaixa extends React.Component {
 
-    state = {
-        busca: "",
-        produtos: [],
-        produtoSelecionado: null,
-        carregando: false
-    };
+    constructor(props) {
+        super(props);
 
-    handleChangeBusca = (event) => {
-        this.setState({ busca: event.target.value });
-    };
+        this.state = {
+            buscaProduto: '',
+            produtos: [],
+            produtoSelecionado: null,
+            carregando: false,
+            preco: null, // mover o preco para fora do produtoSelecionado
+            produtosSelecionados: [],
+            precoProdutoSelecionado: '',
+            quantidade: 1,
+        };
+    }
 
-    buscarProdutos = () => {
-        const { busca } = this.state;
-        console.log("Buscando por:", busca);
-        this.setState({ carregando: true });
+    buscarProdutos = (value) => {
+        console.log("Buscando produto por:", value);
+        this.setState({ buscaProduto: value, carregando: true });
         fetch(`http://localhost:8081/api/v1/produtos`)
-            .then((resposta) => resposta.json())
+            .then((resposta) => {
+                if (!resposta.ok) {
+                    throw new Error('Erro na chamada da API');
+                }
+                return resposta.json();
+            })
             .then((dados) => {
                 if (dados.retorno.produtos) {
                     const produtosFiltrados = dados.retorno.produtos.filter(
                         (produto) =>
-                            (produto.produto.descricao && produto.produto.descricao.toLowerCase().includes(busca.toLowerCase())) ||
-                            (produto.produto.codigo && produto.produto.codigo.toLowerCase().includes(busca.toLowerCase())) ||
-                            (produto.produto.gtin && produto.produto.gtin.toLowerCase().includes(busca.toLowerCase())) ||
-                            (produto.produto.gtinEmbalagem && produto.produto.gtinEmbalagem.toLowerCase().includes(busca.toLowerCase())) ||
-                            (produto.produto.descricaoFornecedor && produto.produto.descricaoFornecedor.toLowerCase().includes(busca.toLowerCase())) ||
-                            (produto.produto.idFabricante && produto.produto.idFabricante.toLowerCase().includes(busca.toLowerCase()))
+                            (produto.produto.descricao && produto.produto.descricao.toLowerCase().includes(value.toLowerCase())) ||
+                            (produto.produto.codigo && produto.produto.codigo.toLowerCase().includes(value.toLowerCase())) ||
+                            (produto.produto.gtin && produto.produto.gtin.toLowerCase().includes(value.toLowerCase())) ||
+                            (produto.produto.gtinEmbalagem && produto.produto.gtinEmbalagem.toLowerCase().includes(value.toLowerCase())) ||
+                            (produto.produto.descricaoFornecedor && produto.produto.descricaoFornecedor.toLowerCase().includes(value.toLowerCase())) ||
+                            (produto.produto.idFabricante && produto.produto.idFabricante.toLowerCase().includes(value.toLowerCase()))
                     );
                     console.log("Objeto retornado:", produtosFiltrados);
                     this.setState({
@@ -60,95 +70,183 @@ class FrenteCaixa extends React.Component {
             });
     };
 
-    cadastraContato = (xmlContato) => {
-        const parser = new DOMParser();
-        const xml = parser.parseFromString(xmlContato, 'text/xml');
-        const stringXml = new XMLSerializer().serializeToString(xml);
+    selecionarProduto = (produto) => {
+        this.setState({
+            produtoSelecionado: produto,
+            preco: produto.produto.preco,
+            produtos: [],
+        });
+        this.atualizarBuscaProduto({ target: { value: '' } });
+    };
 
-        fetch('http://localhost:8080/api/v1/cadastrarcontato', {
-            method: 'POST',
-            body: stringXml,
-            headers: {
-                'Content-Type': 'application/xml'
-            }
+
+
+    adicionarProdutoSelecionado = (produtoSelecionado) => {
+        const { produtosSelecionados, quantidade } = this.state;
+        const produtoExistente = produtosSelecionados.find((produto) => produto.produto.id === produtoSelecionado.produto.id);
+
+        if (produtoExistente) {
+            produtoExistente.quantidade += quantidade; // Adiciona a quantidade selecionada
+        } else {
+            produtosSelecionados.push({
+                produto: produtoSelecionado.produto,
+                quantidade: quantidade, // Salva a quantidade selecionada
+            });
+        }
+
+        this.setState({
+            produtosSelecionados: produtosSelecionados,
+            produtoSelecionado: null,
+            preco: 0,
+            quantidade: 1, // Reinicia o campo de quantidade
+            produtos: [],
+            buscaProduto: '',
+        });
+    };
+
+
+    calcularTotal() {
+        let total = 0;
+        this.state.produtosSelecionados.forEach((produto) => {
+            total += Number(produto.produto.preco);
+        });
+        return total.toFixed(2);
+    }
+
+    atualizaPreco = (event) => {
+        const preco = event.target.value;
+        this.setState({ precoProdutoSelecionado: preco });
+    }
+
+
+    atualizarBuscaProduto = (e) => {
+        this.setState({
+            buscaProduto: e.target.value
         })
     }
 
-    handleSelectProduto = (produto) => {
-        this.setState({ produtoSelecionado: produto });
-    };
+    atualizarQuantidade = (e) => {
+        this.setState({
+            quantidade: e.target.value
+        })
+    }
 
-    calcularSubTotal = () => {
-        const quantidade = parseInt(document.getElementById("quantidade").value);
-        const desconto = parseFloat(document.getElementById("desconto").value || 0);
-        const preco = parseFloat(this.state.produtoSelecionado?.produto?.preco || 0);
-
-        const valorDesconto = preco * (desconto / 100);
-        const valorComDesconto = (preco - valorDesconto) * quantidade;
-
-        const subTotal = valorComDesconto.toFixed(2);
-
-        document.getElementById("subTotal").value = subTotal;
+    atualizarDesconto = (e) => {
+        this.setState({
+            desconto: e.target.value
+        })
     }
 
 
+
     render() {
-        const { produtos, busca, carregando, produtoSelecionado } = this.state;
-
+        const { produtos, produtoSelecionado, buscaProduto, carregando, preco } = this.state;
         return (
-            <div>
-                <input
-                    type="text"
-                    value={busca}
-                    onChange={this.handleChangeBusca}
-                />
-                <button onClick={this.buscarProdutos}>Buscar</button>
-                {carregando ? (
-                    <p>Carregando...</p>
-                ) : produtos.length > 0 ? (
+            <div className="container">
+                <div className="produtos">
                     <div>
-                        <p>Selecione um produto:</p>
-                        <ul>
-                            {produtos.map((produto) => (
-                                <li
-                                    key={produto.produto.codigo}
-                                    onClick={() =>
-                                        this.handleSelectProduto(produto)
-                                    }
-                                >
-                                    {produto.produto.descricao}
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                ) : (
-                    <p>Nenhum produto encontrado</p>
-                )}
+                        <div>
+                            <label htmlFor="produto">Produto</label>
+                            <div>
+                                <input
+                                    type="text"
+                                    id="produto"
+                                    placeholder="Digite a descrição do produto"
+                                    value={buscaProduto}
+                                    onChange={this.atualizarBuscaProduto}
+                                />
+                                <button onClick={() => this.buscarProdutos(buscaProduto)}>Buscar</button>
+                            </div>
+                            <ul>
+                                {produtos.map((produto) => (
+                                    <li key={produto.produto.id} onClick={() => this.selecionarProduto(produto)}>
+                                        {produto.produto.descricao} - R${produto.produto.preco}
+                                    </li>
+                                ))}
+                            </ul>
+                            {produtoSelecionado && (
+                                <div>
+                                    <h2>Produto selecionado: {produtoSelecionado.produto.descricao}</h2>
+                                </div>
+                            )}
+                            <div>
+                                <label htmlFor="quantidade">Quantidade</label>
+                                <input type="number" id="quantidade" name="quantidade" min="1" value={this.state.quantidade} onChange={this.atualizarQuantidade} />
+                            </div>
+                            <label htmlFor="desconto">Desconto</label>
+                            <input type="text" id="desconto" name="desconto" value={this.state.desconto} onChange={this.atualizarDesconto} />
 
-                {produtoSelecionado && (
-                    <div>
-                        <p>Produto selecionado:</p>
-                        <ul>
-                            <li>{produtoSelecionado.produto.descricao}</li>
-                            <li>{produtoSelecionado.produto.codigo}</li>
-                            <li>Preço: R${produtoSelecionado.produto.preco}</li>
-                        </ul>
-                        <label htmlFor="quantidade">Quantidade:</label>
-                        <input type="number" id="quantidade" name="quantidade" onInput={this.calcularSubTotal} />
-                        <br />
-                        <label htmlFor="desconto">Desconto:</label>
-                        <input type="number" id="desconto" name="desconto" defaultValue="0" onInput={this.calcularSubTotal} />
-                        <br />
-                        <label htmlFor="subTotal">Sub Total:</label>
-                        <input type="text" id="subTotal" name="subTotal" disabled />
+                            <div>
+                                <label htmlFor="preco">Preço</label>
+                                <input type="text" id="preco" name="preco" value={preco} onChange={this.atualizaPreco} />
+                                {produtoSelecionado && (
+                                    <button onClick={() => this.adicionarProdutoSelecionado(produtoSelecionado)}>Adicionar</button>
+                                )}
+                            </div>
+                            {carregando && <div>Carregando...</div>}
+                        </div>
                     </div>
-                )}
+                    <div className="carrinho">
+                        <div>
+                            <h2>Total: R${this.calcularTotal()}</h2>
+                            <ul>
+                                {this.state.produtosSelecionados.map((produto) => (
+                                    <li key={produto.produto.id}>
+                                        {produto.produto.descricao} - R${produto.produto.preco}
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
 }
 
 
+
+
+
+
+//                 <div className="form-group">
+//                     <label htmlFor="inputAddress">Address</label>
+//                     <input type="text" className="form-control" id="inputAddress" placeholder="1234 Main St" />
+//                 </div>
+//                 <div className="form-group">
+//                     <label htmlFor="inputAddress2">Address 2</label>
+//                     <input type="text" className="form-control" id="inputAddress2" placeholder="Apartment, studio, or floor" />
+//                 </div>
+//                 <div className="form-row">
+//                     <div className="form-group col-md-6">
+//                         <label htmlFor="inputCity">City</label>
+//                         <input type="text" className="form-control" id="inputCity" />
+//                     </div>
+//                     <div className="form-group col-md-4">
+//                         <label htmlFor="inputState">State</label>
+//                         <select id="inputState" className="form-control">
+//                             <option selected>Choose...</option>
+//                             <option>...</option>
+//                         </select>
+//                     </div>
+//                     <div className="form-group col-md-2">
+//                         <label htmlFor="inputZip">Zip</label>
+//                         <input type="text" className="form-control" id="inputZip" />
+//                     </div>
+//                 </div>
+//                 <div className="form-group">
+//                     <div className="form-check">
+//                         <input className="form-check-input" type="checkbox" id="gridCheck" />
+//                         <label className="form-check-label" htmlFor="gridCheck">
+//                             Check me out
+//                         </label>
+//                     </div>
+//                 </div>
+//                 <button type="submit" className="btn btn-primary">Sign in</button>
+//             </form >
+//         );
+//     }
+// }
 
 export default FrenteCaixa;
 
