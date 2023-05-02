344cf131d63cb826328701319379d50994893969
Rafael Machado <pigao_linkin@hotmail.com>
2023-05-01 23:28:30 -0300

    01/05

--- bling-project/src/components/FrenteCaixa.js
+++ bling-project/src/components/FrenteCaixa.js
@@ -23,13 +23,19 @@ class FrenteCaixa extends React.Component {
 
         this.state = {
             buscaProduto: '',
+            buscaContato: '',
             produtos: [],
+            contatos: [],
             produtoSelecionado: null,
-            carregando: false,
+            contatoSelecionado: null,
+            carregandoProduto: false,
+            carregandoContato: false,
             preco: null, // mover o preco para fora do produtoSelecionado
             produtosSelecionados: [],
+            contatosSelecionados: [],
             precoProdutoSelecionado: '',
             quantidade: 1,
+            cnpj: '',
         };
     }
 
@@ -70,6 +76,54 @@ class FrenteCaixa extends React.Component {
             });
     };
 
+    buscarContato = (value) => {
+        console.log("Buscando contato por:", value);
+        this.setState({ buscaContato: value, carregando: true });
+        fetch(`http://localhost:8080/api/v1/contatos`)
+            .then((resposta) => {
+                if (!resposta.ok) {
+                    throw new Error('Erro na chamada da API');
+                }
+                return resposta.json();
+            })
+            .then((dados) => {
+                if (dados.retorno.contatos) {
+                    const contatosFiltrados = dados.retorno.contatos.filter(
+                        (contato) =>
+                            (contato.contato.nome && contato.contato.nome.toLowerCase().includes(value.toLowerCase())) ||
+                            (contato.contato.cnpj && contato.contato.cnpj.toLowerCase().includes(value.toLowerCase())) ||
+                            (contato.contato.rg && contato.contato.rg.toLowerCase().includes(value.toLowerCase()))
+                    );
+                    console.log("Objeto retornado:", contatosFiltrados);
+                    this.setState({
+                        contatos: contatosFiltrados,
+                        contatoSelecionado: null,
+                        carregando: false,
+                    });
+                } else {
+                    this.setState({ contatos: [], carregando: false });
+                }
+            })
+            .catch((error) => {
+                console.log("Erro ao buscar contatos:", error);
+                this.setState({ contatos: [], carregando: false });
+            });
+    };
+
+    cadastrarPedido = (xmlPedido) => {
+        const parser = new DOMParser();
+        const xml = parser.parseFromString(xmlPedido, 'text/xml');
+        const stringXml = new XMLSerializer().serializeToString(xml);
+
+        fetch('http://localhost:8085/api/v1/cadastrarpedido', {
+            method: 'POST',
+            body: stringXml,
+            headers: {
+                'Content-Type': 'application/xml'
+            }
+        })
+    }
+
     selecionarProduto = (produto) => {
         this.setState({
             produtoSelecionado: produto,
@@ -79,7 +133,14 @@ class FrenteCaixa extends React.Component {
         this.atualizarBuscaProduto({ target: { value: '' } });
     };
 
-
+    selecionarContato = (contato) => {
+        this.setState({
+            contatoSelecionado: contato,
+            cnpj: contato.contato.cnpj,
+            contatos: [],
+        });
+        this.atualizarBuscaContato({ target: { value: '' } });
+    };
 
     adicionarProdutoSelecionado = (produtoSelecionado) => {
         const { produtosSelecionados, quantidade } = this.state;
@@ -104,6 +165,28 @@ class FrenteCaixa extends React.Component {
         });
     };
 
+    adicionarContatoSelecionado = (contatoSelecionado) => {
+        const { contatosSelecionados, cnpj } = this.state;
+        const contatoExistente = contatosSelecionados.find((contato) => contato.contato.id === contatoSelecionado.contato.id);
+
+        if (contatoExistente) {
+            contatoExistente.cnpj += cnpj; // Adiciona a quantidade selecionada
+        } else {
+            contatosSelecionados.push({
+                nome: contatoSelecionado.nome,
+                cnpj: cnpj, // Salva a quantidade selecionada
+            });
+        }
+
+        this.setState({
+            contatosSelecionados: contatosSelecionados,
+            contatoSelecionado: null,
+            cnpj: '',
+            contatos: [],
+            buscaContato: '',
+        });
+    };
+
 
     calcularTotal() {
         let total = 0;
@@ -113,23 +196,28 @@ class FrenteCaixa extends React.Component {
         return total.toFixed(2);
     }
 
-    atualizaPreco = (event) => {
-        const preco = event.target.value;
-        this.setState({ precoProdutoSelecionado: preco });
-    }
-
-
     atualizarBuscaProduto = (e) => {
         this.setState({
             buscaProduto: e.target.value
         })
     }
 
-    atualizarQuantidade = (e) => {
+    atualizarBuscaContato = (e) => {
         this.setState({
-            quantidade: e.target.value
+            buscaContato: e.target.value
         })
     }
+
+    atualizaPreco = (e) => {
+        const preco = Number(e.target.value);
+        this.setState({ preco }, this.atualizarValorTotal);
+    };
+
+    atualizaQuantidade = (e) => {
+        const quantidade = Number(e.target.value);
+        this.setState({ quantidade }, this.atualizarValorTotal);
+    };
+
 
     atualizarDesconto = (e) => {
         this.setState({
@@ -137,24 +225,62 @@ class FrenteCaixa extends React.Component {
         })
     }
 
+    atualizarValorTotal = () => {
+        const { quantidade, preco } = this.state;
+        const valorTotal = quantidade * preco;
+        this.setState({ valorTotal });
+    };
+
+
 
 
     render() {
-        const { produtos, produtoSelecionado, buscaProduto, carregando, preco } = this.state;
+
+        const { produtos, produtoSelecionado, buscaProduto, carregandoProduto, preco, valorTotal } = this.state;
+        const { contatos, contatoSelecionado, buscaContato, carregandoContato, cnpj } = this.state;
+
         return (
+
             <div className="container">
                 <div className="produtos">
                     <div>
                         <div>
-                            <label htmlFor="produto">Produto</label>
                             <div>
-                                <input
-                                    type="text"
-                                    id="produto"
-                                    placeholder="Digite a descrição do produto"
-                                    value={buscaProduto}
-                                    onChange={this.atualizarBuscaProduto}
-                                />
+                                <label htmlFor="produto">Cliente</label>
+                                <input type="text" id="produto" placeholder="Digite a descrição do produto" value={buscaContato} onChange={this.atualizarBuscaContato} />
+                                <button onClick={() => this.buscarContato(buscaContato)}>Buscar</button>
+                            </div>
+                            <ul>
+                                {contatos.map((contato) => (
+                                    <li key={contato.contato.id} onClick={() => this.selecionarContato(contato)}>
+                                        Nome: {contato.contato.nome} - CPF/CNPJ: {contato.contato.cnpj}
+                                    </li>
+                                ))}
+                            </ul>
+                            {contatoSelecionado && (
+                                <div>
+                                    <div>
+                                        <h2>Contato selecionado: {contatoSelecionado.contato.nome}</h2>
+                                    </div>
+                                    <div>
+                                        <label htmlFor="tipo">Tipo</label>
+                                        <input type="text" id="tipo" name="tipo" value={contatoSelecionado.contato.tipo} onChange={this.atualizaQuantidade} />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="cpf">CPF</label>
+                                        <input type="text" id="cpf" name="cpf" value={contatoSelecionado.contato.cnpj} onChange={this.atualizarDesconto} />
+                                    </div>
+                                    <div>
+                                        <label htmlFor="codigo">Código</label>
+                                        <input type="text" id="codigo" name="codigo" value={contatoSelecionado.contato.codigo} onChange={this.atualizaPreco} disabled />
+                                    </div>
+                                </div>
+                            )}
+
+
+                            <div>
+                                <label htmlFor="produto">Produto</label>
+                                <input type="text" id="produto" placeholder="Digite a descrição do produto" value={buscaProduto} onChange={this.atualizarBuscaProduto} />
                                 <button onClick={() => this.buscarProdutos(buscaProduto)}>Buscar</button>
                             </div>
                             <ul>
@@ -169,37 +295,44 @@ class FrenteCaixa extends React.Component {
                                     <h2>Produto selecionado: {produtoSelecionado.produto.descricao}</h2>
                                 </div>
                             )}
-                            <div>
-                                <label htmlFor="quantidade">Quantidade</label>
-                                <input type="number" id="quantidade" name="quantidade" min="1" value={this.state.quantidade} onChange={this.atualizarQuantidade} />
-                            </div>
-                            <label htmlFor="desconto">Desconto</label>
-                            <input type="text" id="desconto" name="desconto" value={this.state.desconto} onChange={this.atualizarDesconto} />
+
 
                             <div>
-                                <label htmlFor="preco">Preço</label>
-                                <input type="text" id="preco" name="preco" value={preco} onChange={this.atualizaPreco} />
+                                <label htmlFor="quantidade">Quantidade</label>
+                                <input type="number" id="quantidade" name="quantidade" min="1" value={this.state.quantidade} onChange={this.atualizaQuantidade} />
+                            </div>
+                            <div>
+                                <label htmlFor="desconto">Desconto</label>
+                                <input type="text" id="desconto" name="desconto" value={this.state.desconto} onChange={this.atualizarDesconto} />
+                            </div>
+                            <div>
+                                <label htmlFor="preco">Valor unitário</label>
+                                <input type="text" id="preco" name="preco" value={preco} onChange={this.atualizaPreco} disabled />
+                            </div>
+                            <div>
+                                <label htmlFor="valorTotal">Sub total</label>
+                                <input type="text" id="valorTotal" name="valorTotal" value={this.state.valorTotal} readOnly />
                                 {produtoSelecionado && (
                                     <button onClick={() => this.adicionarProdutoSelecionado(produtoSelecionado)}>Adicionar</button>
                                 )}
                             </div>
-                            {carregando && <div>Carregando...</div>}
-                        </div>
-                    </div>
-                    <div className="carrinho">
-                        <div>
-                            <h2>Total: R${this.calcularTotal()}</h2>
-                            <ul>
-                                {this.state.produtosSelecionados.map((produto) => (
-                                    <li key={produto.produto.id}>
-                                        {produto.produto.descricao} - R${produto.produto.preco}
-                                    </li>
-                                ))}
-                            </ul>
+                            {carregandoProduto && <div>Carregando...</div>}
                         </div>
                     </div>
                 </div>
-            </div>
+                <div className="carrinho">
+                    <div>
+                        <h2>Total: R${this.calcularTotal()}</h2>
+                        <ul>
+                            {this.state.produtosSelecionados.map((produto) => (
+                                <li key={produto.produto.id}>
+                                    {produto.produto.descricao} - R${produto.produto.preco}
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                </div>
+            </div >
         );
     }
 }
